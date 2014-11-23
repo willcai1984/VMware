9b6bc1d382a6f6b8062d1cd809540a17cd189923
willcai1984 <willcai1984@gmail.com>
2014-11-23 23:24:28 +0800

    update para name

--- scripts/vm_poweroff.py
+++ scripts/vm_poweroff.py
@@ -3,12 +3,12 @@
 # Author: Will
 
 from VMware import VMware
-from VMware.lib.unit import str2list
+from VMware.unit import str2list
 
 def vm_poweroff():
     vm= VMware()
     is_all=vm.connect.value("vm.is_all")
-    name_list = str2list(vm.connect.value("vm.name_list"))
+    name_list = str2list(vm.connect.value("vm.name"))
     if is_all=="true":
         vm.power_off_vm_all()
     else:
