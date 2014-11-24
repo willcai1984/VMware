a4187abf968904116a0bb6edf00dd67d662ac032
willcai1984 <willcai1984@gmail.com>
2014-11-24 21:12:59 +0800

    Add vlan operation

---
+++ scripts/vm_vlandel.py
@@ -0,0 +1,20 @@
+﻿  #!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+# Author: Will
+import re
+from VMware import VMware
+from VMware.unit import str2list
+
+
+def vm_vlandel():
+    vm = VMware()
+    vlan_list = str2list(vm.connect.value("vm.vlan"))
+    vswitch = vm.connect.value("vm.vswitch")
+    int_reg = re.compile('\d+')
+    for vlan in vlan_list:
+        vm.del_vswitch_portgroup(vswitch, vlan)
+        vlan_id = int(int_reg.search(vlan).group())
+        vm.bind_portgroup_vlan(vlan, vlan_id)
+        
+if __name__ == '__main__':
+    vm_vlandel()
