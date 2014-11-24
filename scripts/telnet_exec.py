97904338b2c9eb6f28ba3df7325c8880a5b7a651
willcai1984 <willcai1984@gmail.com>
2014-11-24 21:47:30 +0800

    add telnet exec

---
+++ scripts/telnet_exec.py
@@ -0,0 +1,19 @@
+﻿  #!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+# Author: Will
+
+from VMware import VMware
+from VMware.unit import str2list
+from VMware.expect import Expect
+
+def telnet_exec():
+    vm = VMware()
+    ser_list = str2list(vm.connect.value("vm.serial"))
+    for ser in ser_list:
+        vm = Expect()
+        vm.port = ser
+        vm.telnet_login()
+        vm.basic_exec()
+        vm.basic_logout()
+if __name__ == '__main__':
+    telnet_exec()
