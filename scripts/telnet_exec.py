3c4855981f3d5ba13796b2a4ccc7e7138fcc9347
willcai1984 <willcai1984@gmail.com>
2014-11-25 15:15:50 +0800

    Add default para for telnet exec

--- scripts/telnet_exec.py
+++ scripts/telnet_exec.py
@@ -8,9 +8,15 @@ from VMware.expect import Expect
 def telnet_exec():
     connect = Expect()
     ser_list = str2list(connect.value("vm.serial"))
+    user = connect.value("vm.user")
+    passwd = connect.value("vm.passwd")
+    prompt = connect.value("vm.prompt")
     for ser in ser_list:
         connect = Expect()
         connect.port = ser
+        connect.user = user
+        connect.passwd = passwd
+        connect.prompt = prompt
         connect.telnet_login()
         connect.basic_exec()
         connect.basic_logout()
