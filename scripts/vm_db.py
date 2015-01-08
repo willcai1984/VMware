9593ec9c683ba54113b204aac313e18d653b4aa7
willcai1984 <willcai1984@gmail.com>
2015-01-08 22:15:49 +0800

    update db

--- scripts/vm_db.py
+++ scripts/vm_db.py
@@ -9,7 +9,7 @@ import MySQLdb, time
 def vm_db():
     vm = VMware()
     ip = vm.connect.ip
-    sql_table = 'h' + ip.replace('.', '_')
+    sql_name = vm.connect.value("sql.name")
     sql_ip = vm.connect.value("sql.ip")
     sql_user = vm.connect.value("sql.user")
     sql_passwd = vm.connect.value("sql.passwd")
@@ -22,25 +22,25 @@ def vm_db():
     flag:    0/1 unactive/active
     '''
     create_table_cli="create table if not exists %s (vmid char(10) not null primary key,\
-                      display varchar(200),register varchar(200),power char(1),flag char(1))" % sql_table
+                      display varchar(200),register varchar(200),power char(1),flag char(1))" % sql_name
     cursor.execute(create_table_cli)
-    db_sync(vm, con, cursor, sql_table)
+    db_sync(vm, con, cursor, sql_name)
     cursor.close()
 
-def db_sync(vm, con, cursor, sql_table):
+def db_sync(vm, con, cursor, sql_name):
     vm._data()
-    cursor.execute("update %s set flag='0'" % sql_table)
+    cursor.execute("update %s set flag='0'" % sql_name)
     for i in range(len(vm.id_list)):
         if vm.dis_list[i] in vm.poweron_list:
             cursor.execute("replace into %s(vmid,display,register,power,flag) values('%s','%s','%s','%s','%s')" % 
-                                (sql_table,
+                                (sql_name,
                                 vm.id_list[i],
                                 vm.dis_list[i],
                                 vm.reg_list[i],
                                 '1', '1'))
         else:
             cursor.execute("replace into %s(vmid,display,register,power,flag) values('%s','%s','%s','%s','%s')" % 
-                               (sql_table,
+                               (sql_name,
                                 vm.id_list[i],
                                 vm.dis_list[i],
                                 vm.reg_list[i],
