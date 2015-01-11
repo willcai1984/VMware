590ce944abde8f4676d8e154c6bb82f55798b231
willcai1984 <willcai1984@gmail.com>
2015-01-11 22:51:17 +0800

    update

--- scripts/vm_poweron.py
+++ scripts/vm_poweron.py
@@ -8,12 +8,17 @@ from VMware.unit import str2list
 def vm_poweron():
     vm = VMware()
     is_all = vm.connect.value("vm.is_all")
+    is_id = vm.connect.value("vm.is_id")
     name_list = str2list(vm.connect.value("vm.name"))
-    if is_all=="true":
+    id_list = str2list(vm.connect.value("vm.id"))
+    if is_all == "true":
         vm.power_on_vm_all()
     else:
-        for vmname in name_list:
-            vm.power_on_vm_via_vmname(vmname)
-
+        if is_id == "false":
+            for vmname in name_list:
+                vm.power_on_vm_via_vmname(vmname)
+        elif is_id == "true":
+            for vmid in id_list:
+                vm.power_on_vm_via_vmid(vmid)
 if __name__ == '__main__':
     vm_poweron()
